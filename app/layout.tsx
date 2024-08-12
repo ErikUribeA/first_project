34cd9a797f3eb76dfc9c655aa7250ef434c3c2bc
Erik <erickjosueuribe@gmail.com>
2024-08-12 08:25:19 -0500

    mejoras

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="flex justify-center gap-3">
+        <Link
+          href={'page1'}
+        >Page1</Link>
+        <Link
+          href={'/page2'}>
+          Page2
+        </Link>
+        <Link 
+        href={'/'}
+        >Home
+        </Link>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,6 @@ export default function home(){
   return(
     <>
     <h1>Home</h1>
-    <Link
-    href={'page1'}
-    >Page1</Link>
-    <Link
-    href={'/page2'}>
-      Page2
-    </Link>
     </>
   )
 }
